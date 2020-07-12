04f27e080c5fd6ba999f15d77f6b83a1522cee02
langmap <68081692+langmap@users.noreply.github.com>
2020-07-12 11:27:55 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <Navbar.Brand href="/">
     <img
         alt=""
-        src="./public/small-logo.png"
+        src="https://raw.githubusercontent.com/langmap/langmap/master/public/small-logo.PNG"
         height="70"
         className="d-inline-block align-top"
       />
