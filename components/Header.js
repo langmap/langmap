96fafc6ed0c99a400170e1bfefd43bf320e4f94e
langmap <68081692+langmap@users.noreply.github.com>
2020-07-19 23:30:59 -0400

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,25 @@
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import styles from './Header.module.css';
 
 const Header = () => (
   <Navbar bg="light" variant="light">
     <Navbar.Brand href="/">
     <img
         alt=""
-        src="https://raw.githubusercontent.com/langmap/langmap/master/public/small-logo.PNG"
+        src="./small-logo.png"
         height="70"
         className="d-inline-block align-top"
       />
     </Navbar.Brand>
     <Nav>
-      <Nav.Link href="/" style={{'color': 'black', 'font-size': '1.25em'}}>Home</Nav.Link>
-      <Nav.Link href="/about" style={{'color': 'black', 'font-size': '1.25em'}}>About</Nav.Link>
-      <Nav.Link href="/resources" style={{'color': 'black', 'font-size': '1.25em'}}>Resources</Nav.Link>
-      <Nav.Link href="https://ko-fi.com/langmap" style={{'color': 'black', 'font-size': '1.25em'}}>Donate</Nav.Link>
+      <Nav.Link href="/" className={styles.link}>Home</Nav.Link>
+      <Nav.Link href="/about" className={styles.link}>About</Nav.Link>
+      <NavDropdown title="Resources" id="basic-nav-dropdown" className={styles.link}>
+        <NavDropdown.Item href="/languages" className={styles.link}>Languages</NavDropdown.Item>
+        <NavDropdown.Item href="/linguistics" className={styles.link}>Linguistics</NavDropdown.Item>
+        <NavDropdown.Item href="/geography" className={styles.link}>Geography</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="https://ko-fi.com/langmap" className={styles.link}>Donate</Nav.Link>
     </Nav>
   </Navbar>
 );
