fe28250b752b06021640db8c04d6cb82685bb3bc
langmap <68081692+langmap@users.noreply.github.com>
2020-07-19 23:52:36 -0400

    Add files via upload

--- pages/languages.js
+++ pages/languages.js
@@ -13,7 +13,6 @@ export default function Resources() {
         <Row>
          <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" /> </Col>
          <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" />  </Col>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" /> </Col>
         </Row>
       </Container>
     </Layout>
