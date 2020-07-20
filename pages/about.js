e27134f08aeedb876e00cc016d87da99920e2f98
langmap <68081692+langmap@users.noreply.github.com>
2020-07-20 18:11:46 -0500

    Add files via upload

--- pages/about.js
+++ pages/about.js
@@ -8,16 +8,17 @@ export default function About() {
    	  <h2> What is LangMap? </h2>
       <p>
 	      LangMap is a data visualization tool for language speakers, students and enthusiasts alike. 
-	      Its data comes primarily from Ethnologue with minor supplementary sources. Consider <a href="https://ko-fi.com/langmap">donating to the site</a> to 
+	      Its data comes primarily from <a href="https://www.ethnologue.com/"> Ethnologue </a> with minor supplementary sources. Consider <a href="https://ko-fi.com/langmap">donating to the site</a> to 
 	      keep the servers up and allow us to add new features.
       </p>
       <h2> How many languages are available? </h2>
         <p> 
 	      There are currently 80 languages that can be mapped on LangMap. 
 	      This list includes the top twenty most spoken languages as well as a mix of Indo-European, Semitic, Indo-Aryan, Uralic, Dravidian, Niger-Congo and Sino-Tibetan languages. 
-	      Certain language families have spotty coverage or are so fragmented that visualizing them is uninteresting: e.g., Navajo, which is only spoken in the United States, 
+	      Certain language families have spotty coverage or are so localised that visualizing them on a global scale does not work: e.g., Navajo, which is only spoken in the United States, 
 	      or Quechua, whose dialects are so country-specific that it would be difficult to convey a correct map. 
 	      Certain languages are also more documented than others, and it is difficult to gather data in politically unstable or impoverished areas.
+	      As such, a blank region on the map for a language does not mean no speakers -- it means no data. 
 	    </p>
 	  <h2>What's next for LangMap?</h2>
 	    <p>
@@ -29,7 +30,7 @@ export default function About() {
 	    </p>
       <h2>Where did the idea for LangMap come from?</h2>
 	    <p>
-	      The inspiration for LangMap came from <i>Lingo</i> and <i>Babel</i> by Gaston Dorren: two excellent books about 
+	      The inspiration for LangMap came from <a href="https://amzn.to/30tVJdv"><i>Lingo</i></a> and <a href="https://amzn.to/32CaKwp"><i>Babel</i></a> by Gaston Dorren: two excellent books about 
 	      European languages and the twenty most spoken languages in the world respectively. They are two enjoyable and educational reads, 
 	      and after I finished the books I wanted to explore the international breakdown of language populations in a more visual manner.
 	      It's one thing to read a chart; it's another to read a map.
