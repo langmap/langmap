8a5bf1faae90f7663f5b912da9e3649463930d66
langmap <68081692+langmap@users.noreply.github.com>
2020-07-23 21:36:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import LanguageInput from "../components/LanguageInput";
 function App() {
   return (
   	<Layout>
+      <h2>Explore the world's languages</h2>
+       <p> How many people speak English? Spanish? Swahili? Choose from 80 languages below to find out! </p>
       <LanguageInput/>
      </Layout>
   );
