0b9a5b9b6a8c052e02b5258a3497d10b403a1e92
langmap <68081692+langmap@users.noreply.github.com>
2020-07-26 23:13:10 -0500

    Add files via upload

--- pages/geography.js
+++ pages/geography.js
@@ -6,16 +6,29 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Resources() {
   return (
     <Layout>
-      <h1> Resources </h1>
-      <Meta description={'Language learning materials and more at langmap.me'} url={'https://langmap.me'}/>
-      <h2> German </h2>
+      <center> <h1> Geography Resources </h1> </center>
+      <hr class="solid" style={{  borderTop: '2px solid #bbb', opacity: '0.7', width: '50%'}}/>
+      <Meta description={'Online geography Resources, books and more at langmap.me'} url={'https://langmap.me'}/>
+      <center> <h2> Books </h2> </center>
       <Container>
         <Row>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" /> </Col>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" />  </Col>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" /> </Col>
+         <Col> <ResourceCard description="A fun collection of geography-, map- and history-related trivia from a inveterate traveler and geographer extraordinaire." url="https://amzn.to/32Sq8oG" title="The Trivia Lover's Guide to the World" imgurl="https://images-na.ssl-images-amazon.com/images/I/51960+3ZduL._SX331_BO1,204,203,200_.jpg" /> </Col>
+         <Col> <ResourceCard description="Ten maps that explain everything about geopolitics, from the importance of Crimea to the failures of the Sykes–Picot Agreement to the long-lasting effects of the Monroe Doctrine." url="https://amzn.to/3hQxIEH" title="Prisoners of Geography" imgurl="https://images-na.ssl-images-amazon.com/images/I/51dYH-gcQ7L._SX331_BO1,204,203,200_.jpg" />  </Col>
+         <Col> <ResourceCard description="The story of pivotal episodes in world history -- from the first human migrations out of Africa to the space race -- told through more than 140 detailed maps." url="https://amzn.to/2CGwa13" title="History of the World Map by Map " imgurl="https://images-na.ssl-images-amazon.com/images/I/51XuBaftGVL._SX418_BO1,204,203,200_.jpg" /> </Col>
         </Row>
       </Container>
+
+      <hr class="solid" style={{  borderTop: '2px solid #bbb', opacity: '0.7', width: '90%'}}/>
+
+      <center> <h2> Online Content </h2> </center>
+      <Container>
+        <Row>
+         <Col> <ResourceCard description="A great website for learning country locations, capitals and flags through quizzes." url="https://online.seterra.com/en" title="Seterra" imgurl="https://i.pinimg.com/originals/01/ab/c8/01abc89ebaacdb787e70dfcf2fe77365.png" /> </Col>
+         <Col> <ResourceCard description="GeographyNow is an educational YouTube channel with in-depth videos about every country in the world." url="https://www.youtube.com/user/GeographyNow" title="GeographyNow" imgurl="https://online.seterra.com/images/system/geographynow.png" />  </Col>
+         <Col> <ResourceCard description="Explore the world and guess where you are in this fun online game." url="https://www.geoguessr.com/" title="GeoGuessr" imgurl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXCdUpITAvQlRDETL0dlLpjU1S2k5tlO0u3Q&usqp=CAU" /> </Col>
+        </Row>
+      </Container>
+
     </Layout>
   );
 }
--- pages/linguistics.js
+++ pages/linguistics.js
@@ -6,14 +6,38 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Resources() {
   return (
     <Layout>
-      <h1> Resources </h1>
-      <Meta description={'Language learning materials and more at langmap.me'} url={'https://langmap.me'}/>
-      <h2> German </h2>
+      <center> <h1> Linguistics Resources </h1> </center>
+      <Meta description={'Linguistics books, textbooks and more at langmap.me'} url={'https://langmap.me'}/>
+      <hr class="solid" style={{  borderTop: '2px solid #bbb', opacity: '0.7', width: '50%'}}/>
+
+      <center> <h2> For Fun </h2> </center>
       <Container>
         <Row>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" /> </Col>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" />  </Col>
-         <Col> <ResourceCard description="it's a book" url="https://www.amazon.com/dp/B01N9W111U/" title="German Book 1" imgurl="https://m.media-amazon.com/images/I/51oFXEubSQL._SY346_.jpg" /> </Col>
+         <Col> <ResourceCard description="An entertaining overview of Europe's many languages, ranging from English to Russian to Basque." url="https://amzn.to/30tVJdv" title="Lingo" imgurl="https://images-na.ssl-images-amazon.com/images/I/41NmarPb4BL.jpg" /> </Col>
+         <Col> <ResourceCard description="What is language? John McWhorter explores how languages across the globe originate, evolve, multiply and divide." url="https://amzn.to/3jJQtuY" title="What Language Is" imgurl="https://images-na.ssl-images-amazon.com/images/I/517PbTTVzuL._SX333_BO1,204,203,200_.jpg" /> </Col>
+         <Col> <ResourceCard description="An in-depth look into the history, grammar and demographics of twenty of the world's most spoken languages." url="https://amzn.to/32CaKwp" title="Babel" imgurl="https://images-na.ssl-images-amazon.com/images/I/41oAQDXNRML._SX316_BO1,204,203,200_.jpg" /> </Col>
+        </Row>
+      </Container>
+
+      <hr class="solid" style={{  borderTop: '2px solid #bbb', opacity: '0.7', width: '90%'}}/>
+
+      <center> <h2> Academic Resources </h2> </center>
+      <Container>
+        <Row>
+         <Col> <ResourceCard description="An excellent introductory textbook that samples linguistics' subfields in a fun, casual manner." url="https://amzn.to/2CAfYhH" title="The Study of Language" imgurl="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1370634866l/147521.jpg" />  </Col>
+          <Col> <ResourceCard description="Two essential works on the study of language by Noam Chomsky, the father of modern linguistics." url="https://amzn.to/3g2nyAh" title="On Language: Chomsky's Classic Works" imgurl="https://images-na.ssl-images-amazon.com/images/I/51rT6EasmVL._SX322_BO1,204,203,200_.jpg" />  </Col>
+          <Col> <ResourceCard description="A dense, enlightening read that covers the role of categories, metaphor and analogy across various languages." url="https://amzn.to/3eYgmnb" title="Women, Fire and Dangerous Things" imgurl="https://images-na.ssl-images-amazon.com/images/I/41QEMo5Px1L._SX356_BO1,204,203,200_.jpg" />  </Col>
+        </Row>
+      </Container>
+
+      <hr class="solid" style={{  borderTop: '2px solid #bbb', opacity: '0.7', width: '90%'}}/>
+
+      <center> <h2> Textual Linguistics </h2> </center>
+      <Container>
+        <Row>
+         <Col> <ResourceCard description="The tale of how three lost scripts -- Egyptian hieroglyphs, Maya glyphs, and the Minoan Linear B -- were undeciphered, and a study of six still-uncracked ones." url="https://amzn.to/3f711AS" title="Lost Languages: The Enigma of the World's Undeciphered Scripts" imgurl="https://images-na.ssl-images-amazon.com/images/I/51w8DEhOEFL._SX401_BO1,204,203,200_.jpg" />  </Col>
+          <Col> <ResourceCard description="A humourously relevant book on how the Internet is affecting our use of punctuation, slang, emojis and more." url="https://amzn.to/2CJmSRY" title="Because Internet" imgurl="https://images-na.ssl-images-amazon.com/images/I/41Fj8O9tDXL._SX329_BO1,204,203,200_.jpg" />  </Col>
+          <Col> <ResourceCard description="Where do everyday punctuation marks come from? This chronicle of the parallel histories of language and typography will surprise you." url="https://amzn.to/3g1FAmc" title="Shady Characters: The Secret Life of Punctuation, Symbols & Other Typographical Marks" imgurl="https://images-na.ssl-images-amazon.com/images/I/41R3eOqPNPL._SX343_BO1,204,203,200_.jpg" />  </Col>
         </Row>
       </Container>
     </Layout>
