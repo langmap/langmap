e0e87267e03136e40ff3dffe4fd85a49618bd9ea
langmap <68081692+langmap@users.noreply.github.com>
2020-08-03 20:25:17 -0500

    Add files via upload

---
+++ LanguageInput.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import Map from './Map.js';
+import axios from 'axios';
+import AnimatedNumber from "animated-number-react";
+
+import AsyncSelect from 'react-select/async';
+import { convert, deconvert } from './urlConverter.js';
+
+import Router from 'next/router';
+import {Modal, Button, InputGroup, FormControl} from 'react-bootstrap';
+
+const api_url = 'https://langmap-api.herokuapp.com/';
+
+class LanguageInput extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    var tagObjects = [];
+    if(this.props.code){
+      var tagList = deconvert(this.props.code);
+      for(var i = 0; i < tagList.length; i++){
+        tagObjects.push({value : tagList[i].charAt(0).toUpperCase() + tagList[i].slice(1), label : tagList[i].charAt(0).toUpperCase() + tagList[i].slice(1)});
+      }
+        if (tagObjects.length > 1) {
+        this.multipleLanguageQueries(tagObjects).then((langData) => {
+          this.setState({ langData });
+          let sum = 0;
+          for(var i = 0; i < tagObjects.length; i++){
+              sum += langData[tagObjects[i].value][Object.keys(langData[tagObjects[i].value])[0]].reduce((a, b) => a + (b['val'] || 0), 0);
+          }
+          this.setState({ value : sum});
+        });
+      } else if (tagObjects.length > 0) {
+       this.getLangData(tagObjects[0].value).then((langData) => {
+          this.setState({ langData });
+          this.setState({ value : langData[tagObjects[0].value].reduce((a, b) => a + (b['val'] || 0), 0)});
+        });
+      }
+    }
+
+    this.state = {
+      tags: tagObjects,
+      langData: [],
+      value: 0,
+      emailEntered: false,
+      email: ''
+    };
+
+    this.getLangData = this.getLangData.bind(this);
+    this.multipleLanguageQueries = this.multipleLanguageQueries.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.promiseOptions = this.promiseOptions.bind(this);
+    this.formatValue = this.formatValue.bind(this);
+    this.localizeName = this.localizeName.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+  }
+
+  async getLangData(language) {
+    const response = await axios.get(api_url + 'search/' + language.toLowerCase());
+    return response.data;
+  }
+
+  async multipleLanguageQueries(languages) {
+    let langData = {};
+    for(var i = 0; i < languages.length; i++){
+      const value = await this.getLangData(languages[i].value);
+      langData[languages[i].value] = value;
+    }
+    return langData;
+  }
+
+  handleChange(tags) {
+    if (tags === null){
+      this.setState({ tags : [], langData : [], value : 0 });
+      return;
+    } 
+    this.setState({ tags });
+    if (tags.length > 1) {
+      this.multipleLanguageQueries(tags).then((langData) => {
+        this.setState({ langData });
+        let sum = 0;
+        for(var i = 0; i < tags.length; i++){
+            sum += langData[tags[i].value][Object.keys(langData[tags[i].value])[0]].reduce((a, b) => a + (b['val'] || 0), 0);
+        }
+        this.setState({ value : sum});
+      });
+    } else if (tags.length > 0) {
+     this.getLangData(tags[0].value).then((langData) => {
+        this.setState({ langData });
+        this.setState({ value : langData[tags[0].value].reduce((a, b) => a + (b['val'] || 0), 0)});
+      });
+    }
+    history.pushState(null, null, '/l/' + convert(tags.map(a => a.value)));
+  }
+
+  async promiseOptions(inputValue) {
+    const response = await axios.get(api_url + 'languages');
+    let options = [];
+    for(var i = 0; i < response.data.length; i++) {
+      var name = response.data[i][0].charAt(0).toUpperCase() + response.data[i][0].slice(1) + ' (' + this.localizeName(response.data[i][0]) + ')';
+      options.push({value: response.data[i][0].charAt(0).toUpperCase() + response.data[i][0].slice(1), label: name });
+    }
+    return options;
+  }
+
+  localizeName(name){
+    const names = {
+        'afrikaans': 'Afrikaans', 
+        'albanian': 'Shqipëri', 
+        'amharic': 'አማርኛ', 
+        'armenian': 'հայերեն', 
+        'belarusian' : 'беларуская мова',
+        'azerbaijani': 'Azərbaycan dili', 
+        'bosnian': 'Bosanski', 
+        'cantonese': '廣東話', 
+        'croatian': 'Hrvatski', 
+        'czech': 'Čeština', 
+        'danish': 'Dansk', 
+        'dutch': 'Nederlands', 
+        'estonian': 'Eesti keel', 
+        'persian': 'فارسی', 
+        'finnish': 'Suomen kieli', 
+        'french': 'Français', 
+        'georgian' : 'ქართული',
+        'greek': 'Ελληνικά', 
+        'gujarati': 'ગુજરાતી', 
+        'hausa': 'Harshen Hausa', 
+        'hebrew': 'עברית', 
+        'hindi': 'हिंदी', 
+        'hungarian': 'Magyar nyelv', 
+        'icelandic': 'Íslenska', 
+        'igbo': 'Asụsụ Igbo', 
+        'indonesian': 'Bahasa Indonesia', 
+        'italian': 'Italiano', 
+        'javanese': 'ꦧꦱꦗꦮ', 
+        'kazahk': 'Қазақша', 
+        'khmer': 'ភាសាខ្មែរ', 
+        'korean': '한국어', 
+        'latvian': 'Latviešu valoda', 
+        'lithuanian': 'Lietuvių kalba', 
+        'marathi': 'मराठी', 
+        'hokkien': '閩南語', 
+        'mongolian': 'монгол хэл', 
+        'nepali': 'नेपाली', 
+        'norwegian': 'Norsk', 
+        'odia': 'ଓଡ଼ିଆ', 
+        'punjabi': 'ਪੰਜਾਬੀ', 
+        'romanian': 'Limba română', 
+        'serbian': 'српски', 
+        'spanish' : 'Español',
+        'shona': 'chiShona', 
+        'slovene': 'Slovenščina', 
+        'somali': 'Af Soomaali', 
+        'swedish': 'Svenska', 
+        'tamil': 'தமிழ்', 
+        'telugu': 'తెలుగు', 
+        'thai': 'ภาษาไทย', 
+        'tibetan': 'བོད་སྐད་', 
+        'turkish': 'Türkçe', 
+        'ukrainian': 'українська мова', 
+        'urdu': 'اُردُو', 
+        'uzbek': 'O‘zbekcha', 
+        'vietnamese': 'Tiếng Việt', 
+        'wu': '吳語', 
+        'xhosa': 'isiXhosa', 
+        'yiddish': 'ייִדיש', 
+        'yoruba': 'Èdè Yorùbá', 
+        'zulu': 'isiZulu',
+        'hakka' : '客家話',
+        'kannada' : 'ಕನ್ನಡ',
+        'lao' : 'ພາສາລາວ',
+        'lingala' : 'Lingála',
+        'malay' : 'Bahasa Melayu',
+        'malayam' : 'മലയാളം',
+        'macedonian' : 'македонски',
+        'slovak' : 'Slovenčina',
+        'swahili' : 'Kiswahili',
+        'german' : 'Deutsch',
+        'arabic' : 'اَلْعَرَبِيَّةُ‎',
+        'bengali' : 'বাংলা',
+        'english' : 'English',
+        'mandarin' : '中文',
+        'japanese' : '日本語',
+        'tagalog' : 'Tagalog',
+        'russian' : 'Русский',
+        'portuguese' : 'Português',
+        'polish' : 'Język polski'
+    }
+     return names[name];
+  }
+
+  formatValue(value) {
+    return "You can speak with " + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " people!";
+  }
+
+  async handleEmail() {
+    const response = await axios.get(api_url + 'new/' + this.state.email);
+    console.log(response);
+    this.setState({emailEntered: true});
+    return; 
+  }
+
+  render () {
+    return(
+      <div id="outer-input">
+        <div style={{'marginTop' : '5px', 'marginBottom' : '5px'}}>
+        { this.state.value > 0 && 
+          <h1 align="center"> <AnimatedNumber value={this.state.value} formatValue={this.formatValue} duration="300"/> </h1>
+        }
+        <AsyncSelect cacheOptions placeholder={'Select some languages...'} isSearchable={false} defaultOptions value={this.state.tags} loadOptions={this.promiseOptions} onChange={this.handleChange} isMulti />
+        </div>
+        {(this.state.emailEntered || this.state.value === 0) && 
+          <div>
+            <Map langData={this.state.langData} number={this.state.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/>
+          </div>
+        }
+        {(!this.state.emailEntered && this.state.value !== 0) &&
+          <div>
+              <Modal
+              show={!this.state.emailEntered}
+            onHide={() => {this.setState({emailEntered: true})}}
+            backdrop="static"
+            keyboard={false}
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Get Exclusive LangMap Updates</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Sign up for updates on LangMap, language learning information and more! 
+            </Modal.Body>
+            <InputGroup style={{width: '80%', marginRight: 'auto', marginLeft: 'auto', paddingBottom: '20px'}}>
+              <FormControl placeholder={'email@example.com'} type="email" onChange={(e) => {this.setState({email : e.target.value})}} />
+              <InputGroup.Append>
+                <Button variant="primary" onClick={() => {this.handleEmail()}}>Submit</Button> 
+              </InputGroup.Append>
+            </InputGroup>
+          </Modal>
+          <div style={{ msFilter: 'blur(5px)', filter: 'blur(5px)'}}>
+            <Map langData={this.state.langData} number={this.state.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/>
+          </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+ 
+export default LanguageInput; 
---
+++ MapChart.js
@@ -0,0 +1,180 @@
+import React, { memo } from "react";
+import { scaleLog, scaleThreshold } from "d3-scale";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup
+} from "react-simple-maps";
+import ReactTooltip from 'react-tooltip';
+import { geoPath } from "d3-geo";
+import { geoTimes } from "d3-geo-projection";
+import ShareBlock from './ShareBlock.js';
+import {Button, ButtonGroup, Container, Row, Col} from 'react-bootstrap';
+import ColorBar from './colorBar';
+import WorldMap from './map.json';
+import {isMobile} from 'react-device-detect';
+
+class MapChart extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.mergeData = this.mergeData.bind(this);
+    this.cleanData = this.cleanData.bind(this);
+    this.handleZoomIn = this.handleZoomIn.bind(this);
+    this.handleZoomOut = this.handleZoomOut.bind(this);
+    this.projection = this.projection.bind(this);
+    this.handleGeographyClick = this.handleGeographyClick.bind(this);
+    this.state = {
+      position : { coordinates: [8, 3], zoom: 1 }
+    };
+   }
+
+   mergeData(data){
+	  let mergedData = [];
+	  for(var k = 0; k < Object.keys(data).length; k++) {
+	    for(var j = 0; j < data[Object.keys(data)[k]][Object.keys(data)[k]].length; j++){
+	      if(mergedData.find(item => item.name === data[Object.keys(data)[k]][Object.keys(data)[k]][j].name)){
+	        mergedData.find(item => item.name === data[Object.keys(data)[k]][Object.keys(data)[k]][j].name).val += data[Object.keys(data)[k]][Object.keys(data)[k]][j].val;
+	      } else {
+	        mergedData.push(data[Object.keys(data)[k]][Object.keys(data)[k]][j]);
+	      }
+	    }
+	  }
+	  return mergedData;
+   }
+
+  cleanData(data) {
+    if(typeof data === 'undefined' || Object.keys(data).length === 0 || data === []) return []; 
+    let cleanData = [];
+    for(var k = 0; k < data[Object.keys(data)[0]].length; k++) {
+      cleanData.push(data[Object.keys(data)[0]][k]);
+    }
+    return cleanData;
+  }
+
+   handleZoomIn() {
+    if (this.state.position.zoom >= 6) return;
+    this.setState({position : {coordinates: this.state.position.coordinates, zoom: this.state.position.zoom * 2}});
+  }
+
+   handleZoomOut() {
+    if (this.state.position.zoom <= 1) {
+      this.setState({position : {coordinates: [8, 3], zoom: 1}});
+      return;
+    }
+    this.setState({position : {coordinates: this.state.position.coordinates, zoom: this.state.position.zoom / 2}});
+  }
+
+  handleGeographyClick(geography){
+    const path = geoPath().projection(this.projection());
+    const centroid = this.projection().invert(path.centroid(geography));
+    this.setState({position : {coordinates: centroid, zoom: 4}});
+  };
+
+  projection() {
+    return geoTimes().translate([800 / 2, 450 / 2]).scale(160);
+  };
+
+render () {
+  const layoutStyle = {
+      border: '1px solid #DDD'
+  };
+	let data = Object.keys(this.props.data).length > 1 ? this.mergeData(this.props.data) : this.cleanData(this.props.data);
+	const min = data.length > 0 ? Math.max.apply(Math, data.map(function(o) { return o.val; })) : 0; 
+	const maxValue = data.length > 0 ? Math.min.apply(Math, data.map(function(o) { return o.val; })) : 0;
+	const d = (maxValue - min) / 20; 
+	const customScale = scaleLog()
+        .range(['#006b09', '#00710c', '#027710', '#047e13', '#088416', '#0d8a1a', '#12911d', '#189721', '#1f9d24', '#25a428', '#2caa2c', '#34b02f', '#3cb733', '#45bd37', '#4fc33c', '#5ac940', '#66cf45', '#74d44a', '#86d950', '#a0dc58'])
+        .domain([min + d*1,min + d*2,min + d*3,min + d*4,min + d*5,min + d*6,min + d*7,min + d*8,min + d*9,min + d*10,min + d*11,min + d*12,min + d*13,min + d*14,min + d*15,min + d*16,min + d*17,min + d*18,min + d*19]);
+    return (
+    <div id="outer-div">
+    <ButtonGroup className="float-right">
+            <Button onClick={this.handleZoomIn}>          
+            <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="3"
+                >
+                  <line x1="12" y1="5" x2="12" y2="19" />
+                  <line x1="5" y1="12" x2="19" y2="12" />
+                </svg></Button>
+            <Button onClick={this.handleZoomOut}><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="3"
+                >
+                  <line x1="5" y1="12" x2="19" y2="12" />
+                </svg></Button>
+          </ButtonGroup>
+      <div style={layoutStyle} id="map-outer">
+        <div style={layoutStyle} id="map-inner">
+    <ComposableMap id="the-map" data-tip=""
+            projectionConfig={{
+              rotate: [-10, 0, 0],
+              scale: 147
+            }}
+              width={800}
+        height={370}
+        style={{ width: "100%", height: "auto" }} 
+          >
+          <ZoomableGroup zoom={this.state.position.zoom}
+                center={this.state.position.coordinates} >
+            {data.length >= 0 && (
+              <Geographies geography={WorldMap}>
+                {({ geographies }) => geographies.map((geo, i) => {
+                      const country = data.find(d => d.id === geo.properties.ISO_A3);
+                      return (
+                        <Geography
+                          key={geo.properties.ISO_A3 + i}
+                          geography={ geo }
+                          onClick={() => {this.handleGeographyClick(geo)}}
+                          onMouseEnter={() => {
+                          const { NAME } = geo.properties;
+                          const POP = country ? data.find(element => element.id === geo.properties.ISO_A3).val : 'Few'; 
+                          	this.props.setTooltipContent(`${NAME} — ${POP.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")} Speakers`);
+                          }}
+                          onMouseLeave={() => {
+                          	this.props.setTooltipContent("");
+                          }}
+                          style={{
+                            default: {
+                              fill: country ? customScale(country.val) : "#f8f8ff",
+                              stroke: "#000000",
+                              strokeWidth: 0.75,
+                              outline: "none",
+                            },
+                            hover: {
+                              outline: "none",
+                            },
+                            pressed: {
+                              outline: "none",
+                            }
+                          }}
+                        />
+                      )
+                    })}
+              </Geographies>
+            )}
+          </ZoomableGroup>
+          </ComposableMap>
+          <h2 style={{postion: 'absolute', zIndez: '100', userSelect: 'none', opacity: '0.6', bottom: '0', marginRight: '5px', textAlign: 'right'}}>langmap.me</h2>
+          </div>
+          { Object.keys(this.props.data).length  > 0 && !isMobile && <ColorBar data={data} />}
+        </div>
+        <ShareBlock number={this.props.number} languages={Object.keys(this.props.data)}/>
+      </div>
+    );
+  }
+
+
+
+}
+
+export default React.memo(MapChart);
